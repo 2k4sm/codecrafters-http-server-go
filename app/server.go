cbfc5ca8dbd2a97c2ef5cd763487dd1ab65bcee5
Shrinibas Mahanta <shrinibasmahanta2004@gmail.com>
2024-07-11 23:24:45 +0530

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -1,16 +1,14 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"net"
 	"os"
+	"strings"
 )
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	// fmt.Println("Logs from your program will appear here!")
-
-	// Uncomment this block to pass the first stage
 
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
@@ -22,6 +20,20 @@ func main() {
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
+	}
+	defer conn.Close()
+	reader := bufio.NewReader(conn)
+
+	request, err := reader.ReadString('\n')
+
+	if err != nil {
+		fmt.Println("Error reading data from connection : ", err.Error())
+	}
+
+	requestSlice := strings.Split(request, " ")
+
+	if requestSlice[1] != "/" {
+		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
 	}
 
 	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
