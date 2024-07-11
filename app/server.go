a7f859f262cf28029bd41143a39b8c7a0bbc650f
Shrinibas Mahanta <shrinibasmahanta2004@gmail.com>
2024-07-12 00:24:02 +0530

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -17,11 +17,11 @@ func main() {
 	}
 
 	conn, err := l.Accept()
+	defer conn.Close()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
-	defer conn.Close()
 	reader := bufio.NewReader(conn)
 
 	request, err := reader.ReadString('\n')
@@ -30,11 +30,15 @@ func main() {
 		fmt.Println("Error reading data from connection : ", err.Error())
 	}
 
-	requestSlice := strings.Split(request, " ")
+	requestLine := strings.Split(request, " ")[1]
 
-	if requestSlice[1] != "/" {
+	var path string
+	if requestLine == "/" {
+		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	} else if strings.HasPrefix(requestLine, "/echo/") {
+		path = strings.TrimPrefix(requestLine, "/echo/")
+		conn.Write([]byte(fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(path), path)))
+	} else {
 		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
 	}
-
-	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
 }
